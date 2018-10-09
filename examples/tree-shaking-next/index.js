ff87dd9b003f793a4afb95ed9c8c3660dadb51ed
Kevin Doughty <kvndy@protonmail.com>
2018-10-09 06:22:46 -0400

    removed still more files

--- examples/tree-shaking-next/index.js
+++
@@ -1,4 +0,0 @@
-import { dimensionType } from "../../hyper-animations.mjs";
-// console.log("this is not going to work because variables appear in the wrong order");
-const dimension = dimensionType();
-console.log("dimension type:",dimension);
